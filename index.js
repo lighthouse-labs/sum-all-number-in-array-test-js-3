49b9fc30ba1af67a243a57ef9f30a27d32793e76
monki1 <114178697+monki1@users.noreply.github.com>
2023-04-09 01:37:31 -0400

    init

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let sum = 0;
+  for (let i of array) {
+    if (Array.isArray(i)) {
+      sum += sumItems(i);
+    } else {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
 
 module.exports = sumItems;
